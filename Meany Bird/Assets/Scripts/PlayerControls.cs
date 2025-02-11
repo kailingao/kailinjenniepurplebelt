26b6220f004855be7b8d89fc66f46913e0fcedaf
bbobjedi <dennislittlecat@outlook.com>
2025-02-11 17:59:50 -0500

    rtli,jbc

---
+++ Meany Bird/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    //Game manager object
+    [Header("Game Controller Object for controlling the game")]
+    public GameController gameController;
+    [Header("Velocity")]
+    public float velocity = 5; //Corrected the header to "Velocity"
+    //Physics for the bird
+    private Rigidbody2D rb;
+    //Height of the bird object on the y axis
+    private float objectHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Game Controller component
+        gameController = GetComponent<GameController>();
+        //Speed for the game is at a playing state
+        Time.timeScale = 1;
+        rb = GetComponent<Rigidbody2D>();
+        //Object Height equals the size of the height of the sprite
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+}
+
+
